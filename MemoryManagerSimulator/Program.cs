19720ce04467b3afb421fff8950198d60e5fa852
Lucas <Lklein2@ucs.br>
2019-11-20 20:06:41 -0300

    Adicionar arquivos de projeto.

---
+++ MemoryManagerSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemoryManagerSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
